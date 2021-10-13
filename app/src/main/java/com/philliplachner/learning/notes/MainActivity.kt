b6ce1481764225293d8dc10a40ecbf4ebbdef32d
Mikhail Kirilkin <kirilkin123@gmail.com>
2021-10-13 18:20:06 +0700

    Initial commit

---
+++ app/src/main/java/com/philliplachner/learning/notes/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.philliplachner.learning.notes
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.philliplachner.learning.notes.ui.theme.NotesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NotesTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+
+                }
+            }
+        }
+    }
+}
